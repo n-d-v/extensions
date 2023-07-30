7d055dc024c8a0a5395abef7f48931ec0be4950f
Muffin <muffin@mailbox.org>
2023-07-30 14:06:39 -0500

    Install & configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,27 +12,11 @@ module.exports = {
   globals: {
     Scratch: 'readonly',
     ScratchExtensions: 'readonly',
-    scaffolding: 'readonly'
+    scaffolding: 'readonly',
+    Blockly: 'readonly',
+    ScratchBlocks: 'readonly'
   },
   rules: {
-    // Here is where we enforce rules to have somewhat consistent code style without being overbearing
-    'semi': [
-      'warn',
-      'always'
-    ],
-    'brace-style': 'warn',
-    'key-spacing': 'warn',
-    'keyword-spacing': 'warn',
-    'new-parens': 'warn',
-    'no-trailing-spaces': [
-      'warn',
-      {
-        ignoreComments: true
-      }
-    ],
-    'space-infix-ops': 'warn',
-    'no-tabs': 'warn',
-
     'no-unused-vars': 'off',
     // Allow while (true) { }
     'no-constant-condition': [
